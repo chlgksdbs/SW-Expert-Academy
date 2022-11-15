ab470cedb35e87c3ba69a668ebbcae5ca80c16e8
최한윤 <chlgksdbs98@naver.com>
2022-11-15 23:28:18 +0900

    음수는 양수로 바꾸는 절댓값 함수를 적용하고 최솟값과 그 수를 구하여 출력한 문제 풀이
    - 해당 문제의 채점 사이트에서 파이썬을 적용하지 않아 채점이 불가
    - 댓글에 해당 코드와 같은 코드가 있으므로 정답 처리

---
+++ Difficulty_2/1285.py
@@ -0,0 +1,17 @@
+# 1285. 아름이의 돌 던지기
+# 2022-11-02 (수)
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    n = int(input()) # n : 돌을 던지는 사람의 수
+    stoneLoc = list(map(int, input().split())) # stoneLoc : 각 사람이 돌을 던졌을 때 돌이 떨어진 위치
+
+    for i in range(len(stoneLoc)):
+        if stoneLoc[i] < 0:
+            stoneLoc[i] = abs(stoneLoc[i])
+
+    minValue = min(stoneLoc)
+    minValueCount = stoneLoc.count(minValue)
+
+    print('#', test_case, ' ', minValue, ' ', minValueCount, sep = '')
